94ba04c837931bac0b8baf334e9ea51a547ca77f
Nova <aelithron@gmail.com>
2025-12-16 07:12:07 -0700

    add display backup handing, add more projects, general improvement stuff idk

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,26 +8,27 @@ export default function Page() {
   const topProjects = projectList().filter(project => project.spotlighted);
   const otherProjects = projectList().filter(project => !project.spotlighted);
   return (
-    <div className="flex flex-col min-h-screen p-8 md:p-16">
+    <main className="flex flex-col min-h-screen p-8 md:p-16">
       <h1 className="text-3xl font-semibold"><FontAwesomeIcon icon={faClipboard} /> projects</h1>
-      <div className="flex flex-col mt-2">
+      <p>cool things i&apos;ve built!</p>
+      {topProjects.length > 0 && <div className="flex flex-col mt-2">
         <h2 className="text-xl font-semibold"><FontAwesomeIcon icon={faStar} /> top projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 my-2">
-          {topProjects.map((project, index) => <div key={index}>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-2">
+          {topProjects.map((project, index) => <div key={index} className="p-3 bg-slate-300 dark:bg-slate-700 rounded-lg">
             <h1 className="text-lg font-semibold">{project.title}</h1>
             <p>{project.description}</p>
           </div>)}
         </div>
-      </div>
-      <div className="flex flex-col mt-2">
-        <h2 className="text-xl font-semibold"><FontAwesomeIcon icon={faClipboard} /> other projects</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 my-2">
-          {otherProjects.map((project, index) => <div key={index}>
+      </div>}
+      {otherProjects.length > 0 && <div className="flex flex-col mt-2">
+        {topProjects.length > 0 && <h2 className="text-xl font-semibold"><FontAwesomeIcon icon={faClipboard} /> other projects</h2>}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 my-2">
+          {otherProjects.map((project, index) => <div key={index} className="p-3 bg-slate-300 dark:bg-slate-700 rounded-lg">
             <h1 className="text-lg">{project.title}</h1>
             <p>{project.description}</p>
           </div>)}
         </div>
-      </div>
-    </div>
+      </div>}
+    </main>
   );
 }
--- utils/projects.ts
+++ utils/projects.ts
@@ -2,6 +2,8 @@ import { NovaProject } from "@/novatea";
 
 export default function projectList(): NovaProject[] {
   const projects: NovaProject[] = [];
-  projects.push({ title: "Universal Status", description: "Change your status across many platforms at once!", image: null, spotlighted: true });
+  projects.push({ title: "Universal Status", description: "Change your status across many platforms at once.", image: null, spotlighted: true });
+  projects.push({ title: "LyricKit", description: "A one-stop-shop for finding, writing, and syncing lyrics.", image: null, spotlighted: true });
+  projects.push({ title: "NovaBot", description: "Cusromixable personal assistant Discord bot", image: null, spotlighted: false })
   return projects;
 }
