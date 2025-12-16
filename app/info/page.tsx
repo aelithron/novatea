55ac2d8d7a1291d5469442c41598c08084789247
Nova <aelithron@gmail.com>
2025-12-16 00:35:05 -0700

    start making an info page

---
+++ app/info/page.tsx
@@ -0,0 +1,39 @@
+import { faDiscord, faGithub, faReddit, faSignalMessenger } from "@fortawesome/free-brands-svg-icons";
+import { faComment } from "@fortawesome/free-regular-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Metadata } from "next";
+
+export const metadata: Metadata = { title: "info" }
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-screen p-8 md:p-16">
+      <h1 className="text-3xl font-semibold"><FontAwesomeIcon icon={faComment} /> nova&apos;s accounts!</h1>
+      <p>places where i am / how to contact me :3</p>
+      <div className="flex flex-col mt-3 gap-2">
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1"><FontAwesomeIcon icon={faEnvelope} /> email</div>
+          <a href="mailto:nova@novatea.dev" className="underline hover:text-sky-500">nova@novatea.dev</a>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1"><FontAwesomeIcon icon={faDiscord} /> discord</div>
+          <p>aelithron</p>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1"><FontAwesomeIcon icon={faSignalMessenger} /> signal</div>
+          <p>novatea.84</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 mt-4">
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1"><FontAwesomeIcon icon={faGithub} /> github</div>
+          <a href="https://github.com/aelithron" target="_blank" className="underline hover:text-sky-500">aelithron</a>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex items-center gap-1"><FontAwesomeIcon icon={faReddit} /> reddit</div>
+          <a href="https://reddit.com/u/novatea0" target="_blank" className="underline hover:text-sky-500">novatea0</a>
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/ui.module.tsx
+++ app/ui.module.tsx
@@ -5,12 +5,12 @@ import { faHome, faNewspaper, faClipboard, faComment } from "@fortawesome/free-r
 export function Header() {
   return (
     <div className="flex flex-col md:flex-row items-center md:justify-between gap-2 p-2 md:px-6">
-      <h1 className="text-xl font-semibold bg-linear-to-br from-violet-300 to-violet-500 text-transparent bg-clip-text">nova :3</h1>
+      <Link href={"/"} className="text-xl font-semibold bg-linear-to-br from-violet-300 to-violet-500 text-transparent bg-clip-text">nova :3</Link>
       <div className="flex gap-4 justify-between">
         <Link href={"/"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faHome} /> home</Link>
         <Link href={"/blog"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faNewspaper} /> blog</Link>
         <Link href={"/projects"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faClipboard} /> projects</Link>
-        <Link href={"/contact"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faComment} /> contact</Link>
+        <Link href={"/info"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faComment} /> info</Link>
       </div>
     </div>
   );
