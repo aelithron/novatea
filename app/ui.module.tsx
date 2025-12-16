3bf9b249c5301edd371d6cbc27609697aa1fce27
Nova <aelithron@gmail.com>
2025-12-15 23:21:48 -0700

    header changes forgot to commit earlier :heavysob:

--- app/ui.module.tsx
+++ app/ui.module.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import{ FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHome, faNewspaper, faClipboard } from "@fortawesome/free-regular-svg-icons";
+import { faHome, faNewspaper, faClipboard, faComment } from "@fortawesome/free-regular-svg-icons";
 
 export function Header() {
   return (
     <div className="flex flex-col md:flex-row items-center md:justify-between gap-2 p-2 md:px-6">
-      <h1>nova :3</h1>
+      <h1 className="text-xl font-semibold bg-linear-to-br from-violet-300 to-violet-500 text-transparent bg-clip-text">nova :3</h1>
       <div className="flex gap-4 justify-between">
-        <Link href={"/"} className="hover:text-sky-500"><FontAwesomeIcon icon={faHome} /> home</Link>
-        <Link href={"/blog"} className="hover:text-sky-500"><FontAwesomeIcon icon={faNewspaper} /> blog</Link>
-        <Link href={"/projects"} className="hover:text-sky-500"><FontAwesomeIcon icon={faClipboard} /> projects</Link>
+        <Link href={"/"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faHome} /> home</Link>
+        <Link href={"/blog"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faNewspaper} /> blog</Link>
+        <Link href={"/projects"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faClipboard} /> projects</Link>
+        <Link href={"/contact"} className={`hover:text-sky-500`}><FontAwesomeIcon icon={faComment} /> contact</Link>
       </div>
     </div>
   );
