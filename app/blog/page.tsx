5686cdb97a92804b803a24659df72a898520b2ee
Nova <aelithron@gmail.com>
2025-12-17 23:35:32 -0700

    start building a blog card system thing :3

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,11 @@
 import db from "@/utils/db";
 import { blogTable } from "@/utils/schema";
-import { faNewspaper } from "@fortawesome/free-regular-svg-icons";
-import { faX } from "@fortawesome/free-solid-svg-icons";
+import { faClock, faNewspaper } from "@fortawesome/free-regular-svg-icons";
+import { faArrowRight, faTag, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
+import { ClientTime } from "../clientui.module";
+import Link from "next/link";
 
 export const dynamic = 'force-dynamic';
 export const metadata: Metadata = { title: "blog" };
@@ -29,9 +31,21 @@ async function BlogPosts() {
     );
   }
   return (
-    <div>
-      {blogPosts.map(post => <div key={post.path}>
-        
+    <div className="mt-2">
+      {blogPosts.map(post => <div key={post.path} className="flex p-2 bg-slate-300 dark:bg-slate-800 rounded-lg gap-2 justify-between">
+        <Link href={`/blog/${post.path}`} className="flex gap-2">
+          <div className="flex flex-col">
+            <div className="flex gap-3 items-center">
+              <h1 className="text-lg font-semibold">{post.title}</h1>
+              <p className="text-slate-500"><FontAwesomeIcon icon={faClock} /> <ClientTime date={post.publishedAt} /></p>
+            </div>
+            <p>{post.blurb}</p>
+          </div>
+        </Link>
+        <div className="flex gap-1 items-center">
+          {post.tags.map(tag => <p key={tag} className="bg-slate-400 dark:bg-slate-900 rounded-md p-1"><FontAwesomeIcon icon={faTag} /> {tag}</p>)}
+          <Link href={`/blog/${post.path}`} className="bg-slate-400 dark:bg-slate-900 rounded-full p-1 mx-1 hover:text-sky-500"><FontAwesomeIcon icon={faArrowRight} /></Link>
+        </div>
       </div>)}
     </div>
   )
