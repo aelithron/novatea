a5c5047d31e3ffb2f21c357cb759a137e858aaaa
Nova <aelithron@gmail.com>
2025-12-18 23:30:19 -0700

    attribute thing for input

--- app/auth/auth.form.tsx
+++ app/auth/auth.form.tsx
@@ -26,7 +26,7 @@ export default function AuthPrompt() {
       <h1 className="text-xl font-semibold"><FontAwesomeIcon icon={faUserLock} /> sign in to admin portal</h1>
       <form onSubmit={handleSubmit} className="mt-4 flex flex-col gap-1">
         <label><FontAwesomeIcon icon={faKey} /> admin token:</label>
-        <input value={token} onChange={(e) => setToken(e.target.value)} className="border-violet-500 border-2 rounded-lg p-1" />
+        <input value={token} onChange={(e) => setToken(e.target.value)} className="border-violet-500 border-2 rounded-lg p-1" type="password" />
         <button className="mt-2 bg-slate-300 dark:bg-slate-800 rounded-lg p-1"><FontAwesomeIcon icon={faSignIn} /> sign in</button>
       </form>
     </div>
