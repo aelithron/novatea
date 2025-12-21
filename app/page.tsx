ec4693b068dea5d0f743d33282dfb3c65e56503e
Nova <aelithron@gmail.com>
2025-12-21 02:14:03 -0700

    homepage improvements, blog post widget!

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Metadata } from "next";
 import Image from "next/image";
 import { CopyButton } from "./clientui.module";
-import { UniversalStatusWidget } from "./widgets.module";
+import { UniversalStatusWidget, LatestPostWidget } from "./widgets.module";
 
 export const dynamic = 'force-dynamic';
 export const metadata: Metadata = { title: "home ✧ novatea.dev" };
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen p-8 md:p-16 md:place-content-center items-center">
-      <div className="flex flex-col p-4 bg-slate-800 rounded-xl w-fit">
-        <div className="flex flex-col items-center">
-          <Image src={"https://avatars.githubusercontent.com/u/187228556?v=4"} width={100} height={100} alt="my profile picture" className="rounded-xl" />
-          <h1 className="text-2xl font-semibold flex">hi, i&apos;m <p className="ml-1 bg-linear-to-br from-violet-300 to-violet-500 text-transparent bg-clip-text">nova</p>!</h1>
-          <h2 className="text-lg">(they/she)</h2>
-        </div>
-        <div className="mt-4 gap-2">
-          <p>i&apos;m a full stack dev, writer, and amateur photographer :3</p>
-          <div className="flex gap-2 justify-start text-xl">
-            <a href="mailto:nova@novatea.dev" target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faEnvelope} /></a>
-            <a href="https://github.com/aelithron" target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faGithub} /></a>
-            <CopyButton text="aelithron" message="copied discord username!"><FontAwesomeIcon icon={faDiscord} /></CopyButton>
-            <CopyButton text="novatea.84" message="copied signal username!"><FontAwesomeIcon icon={faSignalMessenger} /></CopyButton>
-            <a href="https://reddit.com/novatea0" target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faReddit} /></a>
+    <main className="flex flex-col min-h-screen p-8 md:p-12">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <div className="flex flex-col gap-2 p-4 bg-slate-300 dark:bg-slate-800 rounded-xl md:w-fit">
+          <div className="flex flex-col">
+            <Image src={"https://avatars.githubusercontent.com/u/187228556?v=4"} width={100} height={100} alt="my profile picture" className="rounded-xl" />
+            <h1 className="text-2xl font-semibold flex items-center">hi, i&apos;m <p className="ml-1 mt-1 bg-linear-to-br from-violet-300 to-violet-500 text-transparent bg-clip-text">nova</p>! (they/she)</h1>
+          </div>
+          <div className="mt-1 gap-2">
+            <p>i&apos;m a full stack dev, writer, and amateur photographer :3</p>
+            <div className="flex gap-2 justify-start text-xl">
+              <a href="mailto:nova@novatea.dev" target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faEnvelope} /></a>
+              <a href="https://github.com/aelithron" target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faGithub} /></a>
+              <CopyButton text="aelithron" message="copied discord username!"><FontAwesomeIcon icon={faDiscord} /></CopyButton>
+              <CopyButton text="novatea.84" message="copied signal username!"><FontAwesomeIcon icon={faSignalMessenger} /></CopyButton>
+              <a href="https://reddit.com/novatea0" target="_blank" className="hover:text-sky-500"><FontAwesomeIcon icon={faReddit} /></a>
+            </div>
           </div>
         </div>
-        <UniversalStatusWidget email="aelithron@gmail.com" />
+        <div className="flex flex-col gap-2 p-4 bg-slate-300 dark:bg-slate-800 rounded-xl md:w-fit">
+          <UniversalStatusWidget email="aelithron@gmail.com" />
+          <LatestPostWidget />
+        </div>
       </div>
     </main>
   );
--- app/widgets.module.tsx
+++ app/widgets.module.tsx
@@ -1,6 +1,11 @@
-import { faClock } from "@fortawesome/free-regular-svg-icons";
+import { faClock, faNewspaper } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ClientTime } from "./clientui.module";
+import db from "@/utils/db";
+import { blogTable } from "@/utils/schema";
+import { eq } from "drizzle-orm";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 type UniversalStatusBody = { status: string, emoji: string, expiry: string };
 export async function UniversalStatusWidget({ email }: { email: string }) {
@@ -16,9 +21,30 @@ export async function UniversalStatusWidget({ email }: { email: string }) {
   if (status.status === "No status set.") return null;
   return (
     <div className="flex flex-col p-2 bg-slate-700 rounded-xl mt-2">
-      <h1 className="font-semibold">nova&apos;s status</h1>
+      <h1 className="font-semibold text-lg">nova&apos;s status</h1>
       <p>{status.emoji} {status.status}</p>
       {status.expiry && <p className="text-sm text-slate-500"><FontAwesomeIcon icon={faClock} /> expires <ClientTime date={new Date(status.expiry)} /></p>}
     </div>
   )
+}
+export async function LatestPostWidget() {
+  let data;
+  try {
+    data = await db.select().from(blogTable).where(eq(blogTable.published, true));
+  } catch (e) {
+    console.warn(`error with database! ${e}`);
+    return null;
+  }
+  if (data.length < 1) return null;
+  return (
+    <Link href={`/blog/${data[0].path}`} className="flex items-center justify-between p-2 bg-slate-700 rounded-xl mt-2">
+      <div className="flex flex-col">
+        <h1 className="font-semibold text-lg"><FontAwesomeIcon icon={faNewspaper} /> latest post</h1>
+        <h2 className="font-semibold">{data[0].title}</h2>
+        <p className="italic text-sm">{data[0].blurb}</p>
+        <p className="text-sm text-slate-500 mt-1"><FontAwesomeIcon icon={faClock} /> <ClientTime date={data[0].publishedAt} /></p>
+      </div>
+      <FontAwesomeIcon icon={faArrowRight} />
+    </Link>
+  );
 }
